0bf52b205f05ec665a741c9f24773ae9761115a4
Nijiko Yonskai <me@nijikokun.com>
2011-05-13 21:41:00 -0400

    Cleanup & Fix checkDisabled

--- src/com/nijikokun/register/payment/Methods.java
+++ src/com/nijikokun/register/payment/Methods.java
@@ -1,51 +1,36 @@
 package com.nijikokun.register.payment;
 
-
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 
 public class Methods {
-    private Method Method = null;
-    
-    public boolean setMethod(Plugin method) {
-        //PluginManager loader = method.getServer().getPluginManager();
 
-        if(method.isEnabled()) {
-            Method pluginMethod = MethodFactory.createMethod(method);
-            if (pluginMethod != null) {
-                Method = pluginMethod;
-            }
+    private Method Method = null;
+
+    public boolean setMethod(Plugin method) {
+        if (method.isEnabled()) {
+            Method plugin = MethodFactory.createMethod(method);
+            if (plugin != null) Method = plugin;
         }
-        
-        /*if(!hasMethod()) {
-            if(loader.getPlugin("iConomy") != null) {
-                method =  loader.getPlugin("iConomy");
-                if(method.getClass().getName().equals("com.iConomy.iConomy"))
-                    Method = new iCo5((iConomy)method);
-                else { Method = new iCo4((com.nijiko.coelho.iConomy.iConomy)method); }
-            } else if(loader.getPlugin("BOSEconomy") != null) {
-                method = loader.getPlugin("BOSEconomy");
-                Method = new BOSE((BOSEconomy)method);
-            } else if(loader.getPlugin("Essentials") != null) {
-                method = loader.getPlugin("Essentials");
-                Method = new EE17((Essentials)method);
-            }
-        }*/
-        
+
         return hasMethod();
     }
 
     public boolean checkDisabled(Plugin method) {
-        PluginDescriptionFile info = method.getDescription();
-        String name = info.getName();
+        if(!hasMethod()) return true;
 
-        if(name.equalsIgnoreCase("iconomy")) {
-            if(method.getClass().getName().equals("com.iConomy.iConomy"))
+        PluginDescriptionFile info = method.getDescription();
+        String name = info.getName(), current = Method.getName();
+
+        if (name.equalsIgnoreCase("iconomy") && current.equalsIgnoreCase("iconomy")) {
+            if (method.getClass().getName().equals("com.iConomy.iConomy") && Method.getVersion().equals("5")) {
                 Method = null;
-            else { Method = null; }
-        } else if(name.equalsIgnoreCase("boseconomy")) {
+            } else {
+                Method = null;
+            }
+        } else if (name.equalsIgnoreCase("boseconomy") && current.equalsIgnoreCase("boseconomy")) {
             Method = null;
-        } else if(name.equalsIgnoreCase("essentials")) {
+        } else if (name.equalsIgnoreCase("essentials") && current.equalsIgnoreCase("essentialseco")) {
             Method = null;
         }
 
@@ -59,5 +44,4 @@ public class Methods {
     public Method getMethod() {
         return Method;
     }
-
 }
