8cf2085b54ebad62d4d60f9f9257b6303276ad8e
Nijiko Yonskai <me@nijikokun.com>
2011-05-13 23:04:55 -0400

    Update example to compensate for a plugin loading last.

--- src/com/nijikokun/register/examples/MyPlugin.java
+++ src/com/nijikokun/register/examples/MyPlugin.java
@@ -16,10 +16,16 @@ public class MyPlugin extends JavaPlugin {
 
     // This is public so we can
     public Method Method = null;
+    public Methods Methods = null;
 
     public void onDisable() { }
 
     public void onEnable() {
+        this.Methods = new Methods();
+
+        // Check before events, incase your plugin loads last.
+        this.Methods.checkDisabled(null);
+
         // Watch plugins enable / disable
         pluginManager.registerEvent(Event.Type.PLUGIN_ENABLE, new server(this), Priority.Monitor, this);
         pluginManager.registerEvent(Event.Type.PLUGIN_DISABLE, new server(this), Priority.Monitor, this);
--- src/com/nijikokun/register/examples/listeners/server.java
+++ src/com/nijikokun/register/examples/listeners/server.java
@@ -18,19 +18,16 @@ public class server extends ServerListener {
     // I would change "MyPlugin" to "Register"
     private MyPlugin plugin;
 
-    // We only need it here.
-    private Methods Methods;
-
     public server(MyPlugin plugin) {
         this.plugin = plugin;
-        this.Methods = new Methods();
+        this.plugin.Methods = new Methods();
     }
 
     @Override
     public void onPluginDisable(PluginDisableEvent event) {
         // Check to see if the plugin thats being disabled is the one we are using
-        if (this.Methods != null && this.Methods.hasMethod()) {
-            Boolean check = this.Methods.checkDisabled(event.getPlugin());
+        if (this.plugin.Methods != null && this.plugin.Methods.hasMethod()) {
+            Boolean check = this.plugin.Methods.checkDisabled(event.getPlugin());
 
             if(check) {
                 System.out.println("[" + plugin.info.getName() + "] Payment method was disabled. No longer accepting payments.");
@@ -41,11 +38,11 @@ public class server extends ServerListener {
     @Override
     public void onPluginEnable(PluginEnableEvent event) {
         // Check to see if we need a payment method
-        if (!this.Methods.hasMethod()) {
-            if(this.Methods.setMethod(event.getPlugin())) {
+        if (!this.plugin.Methods.hasMethod()) {
+            if(this.plugin.Methods.setMethod(event.getPlugin())) {
                 // You might want to make this a public variable inside your MAIN class public Method Method = null;
                 // then reference it through this.plugin.Method so that way you can use it in the rest of your plugin ;)
-                this.plugin.Method = this.Methods.getMethod();
+                this.plugin.Method = this.plugin.Methods.getMethod();
                 System.out.println("[" + plugin.info.getName() + "] Payment method found (" + this.plugin.Method.getName() + " version: " + this.plugin.Method.getVersion() + ")");
             }
         }
