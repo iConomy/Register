fade02e677fb58d4428741c40fae4af32604727d
Nijiko Yonskai <me@nijikokun.com>
2011-05-15 09:31:42 -0400

    Merge MethodFactory into Methods, add Javadoc header with licensing.

--- src/com/nijikokun/register/payment/Methods.java
+++ src/com/nijikokun/register/payment/Methods.java
@@ -1,27 +1,64 @@
 package com.nijikokun.register.payment;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
+/**
+ * Methods.java
+ * Controls the getting / setting of methods & the method of payment used.
+ *
+ * @author: Nijikokun<nijikokun@gmail.com> (@nijikokun)
+ * @copyright: Copyright (C) 2011
+ * @license: GNUv3 Affero License <http://www.gnu.org/licenses/agpl-3.0.html>
+ */
 public class Methods {
 
     private Method Method = null;
+    private Set<Method> Methods = new HashSet<Method>();
+    private Set<String> Dependencies = new HashSet<String>();
 
     public Methods() {
-        MethodFactory.addMethod("iConomy", new com.nijikokun.register.payment.methods.iCo4());
-        MethodFactory.addMethod("iConomy", new com.nijikokun.register.payment.methods.iCo5());
-        MethodFactory.addMethod("BOSEconomy", new com.nijikokun.register.payment.methods.BOSE());
-        MethodFactory.addMethod("Essentials", new com.nijikokun.register.payment.methods.EE17());
+        this.addMethod("iConomy", new com.nijikokun.register.payment.methods.iCo4());
+        this.addMethod("iConomy", new com.nijikokun.register.payment.methods.iCo5());
+        this.addMethod("BOSEconomy", new com.nijikokun.register.payment.methods.BOSE());
+        this.addMethod("Essentials", new com.nijikokun.register.payment.methods.EE17());
+    }
+
+    public Set<String> getDependencies() {
+        return Dependencies;
+    }
+
+    public Method createMethod(Plugin plugin) {
+        for (Method method: Methods) {
+            if (method.isCompatible(plugin)) {
+                method.setPlugin(plugin);
+                return method;
+            }
+        }
+
+        return null;
+    }
+
+    private void addMethod(String name, Method method) {
+        Dependencies.add(name);
+        Methods.add(method);
+    }
+
+    public boolean hasMethod() {
+        return (Method != null);
     }
 
     public boolean setMethod(Plugin method) {
         PluginManager manager = method.getServer().getPluginManager();
 
         if (method != null && method.isEnabled()) {
-            Method plugin = MethodFactory.createMethod(method);
+            Method plugin = this.createMethod(method);
             if (plugin != null) Method = plugin;
         } else {
-            for(String name: MethodFactory.getDependencies()) {
+            for(String name: this.getDependencies()) {
                 if(hasMethod()) break;
 
                 method = manager.getPlugin(name);
@@ -29,7 +66,7 @@ public class Methods {
                 if(!method.isEnabled()) manager.enablePlugin(method);
                 if(!method.isEnabled()) continue;
 
-                Method plugin = MethodFactory.createMethod(method);
+                Method plugin = this.createMethod(method);
                 if (plugin != null) Method = plugin;
             }
         }
@@ -37,17 +74,13 @@ public class Methods {
         return hasMethod();
     }
 
+    public Method getMethod() {
+        return Method;
+    }
+
     public boolean checkDisabled(Plugin method) {
         if(!hasMethod()) return true;
         if (Method.isCompatible(method)) Method = null;
         return (Method == null);
-    }
-
-    public boolean hasMethod() {
-        return (Method != null);
-    }
-
-    public Method getMethod() {
-        return Method;
     }
 }
